202d04fb7793bbf5bac7a4d94c7cd802a92bb40c
Hidrid-kel <106839351+Hidrid-kel@users.noreply.github.com>
2024-01-02 16:15:35 +0700

    add a restart button

--- MineSweeper-DSA/src/Minesweeper.java
+++ MineSweeper-DSA/src/Minesweeper.java
@@ -9,12 +9,27 @@ public class Minesweeper extends MineFunctions {
   JPanel textPanel = new JPanel();
   JPanel boardPanel = new JPanel();
 
+  JButton restartButton;
+  JPanel controlPanel = new JPanel();
+
   Minesweeper() {
     frame.setSize(boardWidth, boardHeight);
     frame.setLocationRelativeTo(null);
     frame.setResizable(false);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setLayout(new BorderLayout());
+
+    restartButton = new JButton("Restart");
+    restartButton.addActionListener(
+      new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          resetGame();
+        }
+      }
+    );
+    controlPanel.add(restartButton);
+    frame.add(controlPanel, BorderLayout.SOUTH);
 
     textLabel.setFont(new Font("Arial", Font.BOLD, 25));
     textLabel.setHorizontalAlignment(JLabel.CENTER);
@@ -73,6 +88,56 @@ public class Minesweeper extends MineFunctions {
     }
 
     frame.setVisible(true);
+
+    setMines();
+  }
+
+  void resetGame() {
+    boardPanel.removeAll();
+    for (int r = 0; r < numRows; r++) {
+      for (int c = 0; c < numCols; c++) {
+        MineTile tile = new MineTile(r, c);
+        board[r][c] = tile;
+        tile.setFocusable(false);
+        tile.setMargin(new Insets(0, 0, 0, 0));
+        tile.setFont(new Font("Arial Unicode MS", Font.PLAIN, 45));
+        tile.addMouseListener(
+          new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+              if (gameOver) {
+                return;
+              }
+              MineTile tile = (MineTile) e.getSource();
+
+              if (e.getButton() == MouseEvent.BUTTON1) {
+                if (tile.getText() == "") {
+                  if (mineList.contains(tile)) {
+                    revealMines();
+                  } else {
+                    checkMine(tile.r, tile.c);
+                  }
+                }
+              } else if (e.getButton() == MouseEvent.BUTTON3) {
+                if (tile.getText() == "" && tile.isEnabled()) {
+                  tile.setText("F");
+                } else if (tile.getText() == "F") {
+                  tile.setText("");
+                }
+              }
+            }
+          }
+        );
+        boardPanel.add(tile);
+      }
+    }
+
+    boardPanel.revalidate();
+    boardPanel.repaint();
+
+    tilesClicked = 0;
+    gameOver = false;
+    textLabel.setText("Minesweeper: " + Integer.toString(mineCount));
 
     setMines();
   }
